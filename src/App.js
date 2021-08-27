ec78b45ebe2cea2966fdc7254a74486bc25be5ed
Bryant Fukushima <bmfukushima94@gmail.com>
2021-08-26 18:43:29 -0700

    creates(pages & nav): create page & nav files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
+import NewMeetupsPage from './pages/NewMeetup';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetups'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
